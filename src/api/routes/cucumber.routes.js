d323d9cbdaa05f972c1263e8dcac9a7bdd955c00
IvanBorras <160221335+IvanBorras@users.noreply.github.com>
2024-05-03 11:35:17 +0200

    Update cucumber.routes.js

--- src/api/routes/cucumber.routes.js
+++ src/api/routes/cucumber.routes.js
@@ -1,11 +1,12 @@
-// cucumber.routes.js
 const express = require('express');
 const router = express.Router();
 const cucumberController = require('../controllers/cucumber.controllers');
+const { isAuth } = require('../../middleware/auth');
 
-router.post('/create', cucumberController.createCucumber);
-router.get('/list', cucumberController.getCucumbers);
+// Ruta para crear un nuevo pepino (requiere autenticación)
+router.post('/create', isAuth, cucumberController.createCucumber);
 
-// Agrega más rutas según sea necesario
+// Ruta para obtener la lista de pepinos de un agricultor específico (requiere autenticación)
+router.get('/list', isAuth, cucumberController.getCucumbers);
 
 module.exports = router;
