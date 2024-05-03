4a7b3c3de77f40f9cf8641b9cea592c6dfac5a36
IvanBorras <160221335+IvanBorras@users.noreply.github.com>
2024-05-03 11:44:39 +0200

    Update jwt.js

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,15 +1,15 @@
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken"); // Importa el módulo jsonwebtoken para trabajar con tokens JWT
 
-const generateToken = (data) => {
-    //data, informacion a guardar en el token
-    //secreteKeyPepino , clave secreta que me invento
-    //{ expiresIn: '1h' } tiempo de duracion del token
-    return jwt.sign(data, process.env.SECRET_JWT, { expiresIn: '1h' })
+// Función para generar un token JWT
+const generateToken = (data) => {     // 'data' es la información que se guardará en el token
+    return jwt.sign(data, process.env.SECRET_JWT, { expiresIn: '1h' });  // 'process.env.SECRET_JWT' es la clave secreta utilizada para firmar el token, recuperada de las variables de entorno. { expiresIn: '1h' } define el tiempo de expiración del token.
 }
+
+// Función para verificar y decodificar un token JWT
 const verifyToken = (token) => {
-    console.log(token)
-    return jwt.verify(token, "secreteKeyPepino")
-
+    console.log(token); // Muestra el token en la consola (para fines de depuración)
+    return jwt.verify(token, "secreteKeyPepino"); // Verifica y decodifica el token utilizando la clave secreta "secreteKeyPepino"
 }
 
-module.exports = { generateToken, verifyToken }
+// Exporta las funciones 'generateToken' y 'verifyToken' para que puedan ser utilizadas en otras partes de la aplicación
+module.exports = { generateToken, verifyToken };
