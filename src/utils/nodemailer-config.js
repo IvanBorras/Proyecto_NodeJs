d69f8023b617d14cb13bb1ff5f63176fbe9527e5
IvanBorras <160221335+IvanBorras@users.noreply.github.com>
2024-05-03 11:48:49 +0200

    Update nodemailer-config.js

--- src/utils/nodemailer-config.js
+++ src/utils/nodemailer-config.js
@@ -1,15 +1,14 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer'); // Importa el módulo nodemailer para enviar correos electrónicos
 
-
-// Configuración del objeto de transporte
+// Configuración del objeto de transporte para enviar correos electrónicos
 const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    secure: false,
+    host: 'smtp.ethereal.email', // Servidor SMTP para enviar correos electrónicos (en este caso, Ethereal)
+    port: 587, // Puerto del servidor SMTP
+    secure: false, // Si el servidor SMTP utiliza SSL o TLS
     auth: {
-        user: 'amaya.hamill47@ethereal.email',
-        pass: 'nMFrSseGeEHC8FRBFt'
+        user: 'amaya.hamill47@ethereal.email', // Nombre de usuario para autenticarse en el servidor SMTP
+        pass: 'nMFrSseGeEHC8FRBFt' // Contraseña para autenticarse en el servidor SMTP
     }
 });
 
-module.exports = transporter;
+module.exports = transporter; // Exporta el objeto de transporte para que pueda ser utilizado en otras partes de la aplicación
